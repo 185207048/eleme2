4e0df71af3b130e3af722bcd91ef0382c843c50c
leo <185207048@qq.com>
2020-06-30 08:52:44 +0800

    101

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="main">
+        <div class="header">
+        <!--头的左半部分-->
+            <div class="header-left">
+                <div class="header-left-content">
+                    <div class="header-content">
+                        <img class="header-img" :src="require('../assets/image/logo.png')" />
+                        <div class="header-point">·</div>
+                        <div class="header-title">商家开发平台</div>
+                    </div>
+                </div>
+            </div>
+        <!--头的右半部分-->
+            <div class="header-right">
+                <div class="header-right-content">
+                    <div class="header-right-title">首页</div>
+                    <div class="header-right-title">文档中心</div>
+                    <div class="header-right-title">管理中心</div>
+                    <div class="header-right-title">帮助中心</div>
+                    <div class="header-right-input header-right-title">
+                        <input placeholder="搜索"  class="header-input"/>
+                    </div>
+                    <div class="header-right-title">|</div>
+                    <div class="header-right-title">登录</div>
+                    <div class="header-right-title">注册</div>
+                </div>
+            </div>
+
+            <div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        data () {
+            return {
+               
+            }
+        }
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+*{
+    margin:0;
+    padding:0;
+    font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, Tahoma, Arial, "PingFang SC", "Heiti SC", "Microsoft YaHei", SimHei, "WenQuanYi MicroHei", sans-serif !important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.header{
+    background-color: brown;
+    display: flex;
+    padding:0.1rem 0.5rem;
+    min-height: 0.2rem;
+    justify-content:center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.header-left{
+    display: flex;
+    flex-direction: row;
+    min-height: 0.15rem;
 }
-a {
-  color: #42b983;
+.header-img{
+    height: 0.13rem;
+}
+.header-content{
+    align-items:center;
+    font-size: 0.15rem;
+    display: flex;
+}
+.header-point{
+    margin:0 0.05rem;
+    color:white
+}
+.header-title{
+    font-size:0.10rem;
+    align-items: center;
+    color:white
+}
+.header-right{
+    float: right;
+}
+.header-right-content{
+    display: flex;
+    color:white;
+    font-size:0.10rem;
+    padding-top:0.025rem ;
+    justify-content:flex-end;
+    width: 5.5rem;
+}
+.header-right-input{
+    width: 0.5rem;
+    height: 0.15rem;
+}
+.header-right-title{
+    margin-right: 0.25rem;
+    cursor: pointer;
+}
+.header-input{
+    width: 0.5rem;
+    background-color: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'amfe-flexible'
+import Element from 'element-ui'
+import '../element-variables.scss'
+Vue.use(Element)
 
 Vue.config.productionTip = false
 
